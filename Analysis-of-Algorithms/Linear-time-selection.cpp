4822c48b03412c6e255167bcdf0c5b03f5c04d27
lalalaxuzhengfei <3349297129@qq.com>
2020-06-23 15:03:30 +0800

    update

--- Analysis-of-Algorithms/Linear-time-selection.cpp
+++ Analysis-of-Algorithms/Linear-time-selection.cpp
@@ -1,105 +1,97 @@
-// 油井问题，线性时间选择算法
-#include <cstdio>
-using namespace std;
-
-#define MAX 2000010
+#include <stdio.h>
+#define MAX 100000
 int num[MAX];
-class Linear_select
+// 选择排序
+void slsort(int p, int q)
 {
-public:
-    // 选择排序
-    void slsort(int p, int q)
+    for (int i = p + 1; i <= q; i++)
     {
-        for (int i = p + 1; i <= q; i++)
+        int temp = num[i], j = i - 1;
+        while (j >= p)
         {
-            int temp = num[i], j = i - 1;
-            while (j >= p)
+            if (num[j] > temp)
             {
-                if (num[j] > temp)
-                {
-                    num[j + 1] = num[j];
-                    j--;
-                }
-                else
-                    break;
-            }
-            num[j + 1] = temp;
-        }
-    }
-    // 分解函数
-    int Partition(int p, int q, int mid)
-    {
-        int i = p, j = q;
-        while (i <= q && j >= p)
-        {
-            while (num[i] < mid)
-            {
-                i++;
-            }
-            while (num[j] > mid)
-            {
+                num[j + 1] = num[j];
                 j--;
             }
-            if (i >= j)
-            {
-                break;
-            }
             else
-            {
-                int temp = num[i];
-                num[i] = num[j];
-                num[j] = temp;
-                i++, j--;
-            }
+                break;
         }
-        return j;
+        num[j + 1] = temp;
     }
-    // 选择函数
-    int Select(int p, int q, int k)
+}
+// 分解函数
+int Partition(int p, int q, int mid)
+{
+    int i = p, j = q;
+    while (i <= q && j >= p)
     {
-        if (q - p < 10)
+        while (num[i] < mid)
         {
-            slsort(p, q);
-            return num[p + k - 1];
+            i++;
         }
-        for (int i = 0; i <= (q - p - 4) / 5; i++)
+        while (num[j] > mid)
         {
-            slsort(p + 5 * i, p + 5 * i + 4);
-            int temp = num[p + 5 * i + 2];
-            num[p + 5 * i + 2] = num[p + i];
-            num[p + i] = temp;
+            j--;
         }
-        int mid = Select(p, p + (q - p - 4) / 5, ((q - p - 4) / 5 + 1) / 2);
-        int mid_id = Partition(p, q, mid);
-        int mid_rank = mid_id - p + 1;
-        if (k == mid_rank)
-        {
-            return num[mid_id];
-        }
-        else if (k < mid_rank)
-        {
-            return Select(p, mid_id, k);
-        }
+        if (i >= j)
+            break;
         else
         {
-            return Select(mid_id + 1, q, k - mid_rank);
+            int temp = num[i];
+            num[i] = num[j];
+            num[j] = temp;
+            i++, j--;
         }
     }
-};
-int main(int argc, char const *argv[])
+    return j;
+}
+// 选择函数
+int Select(int p, int q, int k)
 {
-    int i = 0, x;
-    while (scanf("%d,%d", &x, &num[i]) != EOF)
+    if (q - p < 75)
     {
-        i++;
+        slsort(p, q);
+        return num[p + k - 1];
     }
-    if (i % 2 == 0)
+    // 选出 n/5 组中每个组的中位数
+    for (int i = 0; i <= (q - p - 4) / 5; i++)
     {
-        printf("%d\n", Select(0, i - 1, i / 2));
+        slsort(p + 5 * i, p + 5 * i + 4);
+        int temp = num[p + 5 * i + 2];
+        num[p + 5 * i + 2] = num[p + i];
+        num[p + i] = temp;
+    }
+    // 选出各种中位数的中位数 mid
+    int mid = Select(p, p + (q - p - 4) / 5, ((q - p - 4) / 5 + 1) / 2);
+    // 以 mid 为基准进行分解
+    int mid_id = Partition(p, q, mid);
+    int mid_rank = mid_id - p + 1;
+    // 递归条件判断
+    if (k == mid_rank)
+    {
+        return num[mid_id];
+    }
+    else if (k < mid_rank)
+    {
+        return Select(p, mid_id, k);
     }
     else
     {
-        printf("%d\n", Select(0, i - 1, i / 2 + 1));
+        return Select(mid_id + 1, q, k - mid_rank);
     }
+}
+int main(int argc, char const *argv[])
+{
+    int i = 0, k;
+    while (scanf("%d", &num[i]) != EOF)
+    {
+        i++;
+    }
+    scanf("%d", &k); // 选择第几小的元素
+    if (k > i)
+        printf("error!\n");
+    else
+        printf("%d\n", Select(0, i, k));
     return 0;
 }
